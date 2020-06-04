23377b88d110560808a7ef353bd6ce1b176c4d77
mathiasminacapilli <mathiasminacapilli@gmail.com>
2020-06-04 07:37:22 -0300

    Update links screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,9 @@
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { MonoText } from '../components/StyledText';
 
 export default function LinksScreen() {
   return (
@@ -25,7 +26,20 @@ export default function LinksScreen() {
         onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
         isLastOption
       />
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.helpLinkText}>Developed by </Text>
+        <TouchableOpacity onPress={onPressDevelopedByHandler} style={styles.helpLink}>
+          <Text>Mathias Minacapilli</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
+  );
+}
+
+function onPressDevelopedByHandler() {
+  WebBrowser.openBrowserAsync(
+    'http://mathiasminacapilli.com'
   );
 }
 
@@ -70,5 +84,30 @@ const styles = StyleSheet.create({
     fontSize: 15,
     alignSelf: 'flex-start',
     marginTop: 1,
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
   },
 });
